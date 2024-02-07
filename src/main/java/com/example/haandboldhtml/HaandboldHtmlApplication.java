1d48434f1c84c683a2132f59602a81b6bbfbd505
Asger <asgervb@gmail.com>
2024-02-07 11:17:16 +0100

    Initial commit

---
+++ src/main/java/com/example/haandboldhtml/HaandboldHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.haandboldhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaandboldHtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaandboldHtmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/haandboldhtml/HaandboldHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haandboldhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaandboldHtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
